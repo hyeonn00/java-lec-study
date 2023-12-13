4a7489efa482a92af27074e77c25361337c4b83c
hyeon <tjgushyun@nate.com>
2023-12-13 21:07:43 +0900

    programing ex

---
+++ src/bookEx02/ProgEx05.java
@@ -0,0 +1,18 @@
+package bookEx02;
+
+import java.util.Scanner;
+
+public class ProgEx05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("시간 간격을 입력하시오: ");
+        int sec = sc.nextInt();
+
+        int sound = sec * 340;
+        int flash = sec * 300000;
+        int light = flash / sound / sound;
+
+        System.out.print("번개가 발생한 곳까지의 거리: " + light);
+    }
+}
