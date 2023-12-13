09a7c219084aa2bccc90ba3ff27222b7130d70d5
hyeon <tjgushyun@nate.com>
2023-12-13 09:40:24 +0900

    반지름이 5인 원의 면적

---
+++ src/ex02/Cha02.java
@@ -0,0 +1,12 @@
+package ex02;
+
+public class Cha02 {
+    public static void main(String[] args) {
+        final double PI = 3.121592;
+        double radius, area;
+
+        radius = 5.0;
+        area = PI * radius * radius;
+        System.out.println("반지름이 5인 원의 면적은" + area);
+    }
+}
