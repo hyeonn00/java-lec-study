04418071e5a14d44b286148739c3227aabe1d972
hyeon <tjgushyun@nate.com>
2023-12-14 11:05:30 +0900

    가위바위보

---
+++ src/bookEx02/ProgEx06.java
@@ -0,0 +1,12 @@
+package bookEx02;
+
+import java.util.Scanner;
+
+public class ProgEx06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("드라이브 이름: ");
+        String driveName = sc.nextLine();
+
+    }
+}
