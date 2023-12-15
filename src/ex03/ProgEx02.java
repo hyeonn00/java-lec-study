2066e87b2c90750045f5ef171b7d4a4cef9a698d
hyeon <tjgushyun@nate.com>
2023-12-15 16:38:31 +0900

    버블정렬

--- src/ex03/ProgEx02.java
+++ src/ex03/ProgEx02.java
@@ -5,18 +5,23 @@ import java.util.Scanner;
 public class ProgEx02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("연산을 입력: ");
-        String s = sc.nextLine();
+        System.out.print("연산 입력: ");
+        String s = sc.next();
 
-        System.out.print("피연산 2개를 입력: ");
+        System.out.print("피연산자 2개 입력: ");
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
-        int t1 = n1 + n2;
-        int t2 = n1 - n2;
-        int t3 = n1 * n2;
-        int t4 = n1 / n2;
-        if (s == "*"){
 
+        if (s.equals("+")){
+            System.out.printf("%d + %d = %d", n1,n2,n1+n2);
+        }else if (s.equals("-")){
+            System.out.printf("%d - %d = %d", n1,n2,n1-n2);
+        }else if (s.equals("*")){
+            System.out.printf("%d * %d = %d", n1,n2,n1*n2);
+        }else if (s.equals("/")){
+            System.out.printf("%d / %d = %d", n1,n2,n1/n2);
+        }else {
+            System.out.println("Error");
         }
     }
 }
